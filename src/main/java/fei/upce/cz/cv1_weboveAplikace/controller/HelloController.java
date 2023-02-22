99dc9bc8ba0efe416c233f7349132439c8ebc428
Jan Korbel (st69799) <71786085+duffi4545@users.noreply.github.com>
2023-02-22 23:29:10 +0100

    adding RestController

--- src/main/java/fei/upce/cz/cv1_weboveAplikace/controller/HelloController.java
+++ src/main/java/fei/upce/cz/cv1_weboveAplikace/controller/HelloController.java
@@ -1,4 +1,11 @@
 package fei.upce.cz.cv1_weboveAplikace.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//Jedná se o pohodlnou anotaci, která kombinuje @Controller a @ResponseBody,
+// což eliminuje potřebu anotovat každou metodu zpracování požadavků třídy kontroleru pomocí @ResponseBody anotace
+@RestController
+@RequestMapping("/api/v1")
 public class HelloController {
 }
