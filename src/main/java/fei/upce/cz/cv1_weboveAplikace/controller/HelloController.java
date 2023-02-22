c47d1cc9bc9f978ab5afcbdb6c58f662bb31ddec
Jan Korbel (st69799) <71786085+duffi4545@users.noreply.github.com>
2023-02-22 23:52:14 +0100

    adding pathVariable and requestParam

--- src/main/java/fei/upce/cz/cv1_weboveAplikace/controller/HelloController.java
+++ src/main/java/fei/upce/cz/cv1_weboveAplikace/controller/HelloController.java
@@ -1,11 +1,25 @@
 package fei.upce.cz.cv1_weboveAplikace.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //Jedná se o pohodlnou anotaci, která kombinuje @Controller a @ResponseBody,
 // což eliminuje potřebu anotovat každou metodu zpracování požadavků třídy kontroleru pomocí @ResponseBody anotace
 @RestController
 @RequestMapping("/api/v1")
 public class HelloController {
+    @GetMapping("")
+    public String helloWorld() {
+        return "Hello world from Spring Boot application.";
+    }
+
+    @GetMapping("/path/{message}")
+    public String helloPathParams(@PathVariable String message) {
+        return "Parameter in path: " + message;
+    }
+
+    @GetMapping("/query")
+    public String helloQueryParams(@RequestParam String message) {
+        return "Hello with query params: " + message;
+    }
 }
+
